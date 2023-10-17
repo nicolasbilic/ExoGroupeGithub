47eef65aeb0e4767bcfd38b33dc7c9b870c2f8e2
nicolasbilic <nicolasbilic9@gmail.com>
2023-10-17 15:49:01 +0200

    README.md

---
+++ consignes.js
@@ -0,0 +1,48 @@
+/* 
+
+Chacun crée un repository en local
+
+[15:21] Yvan Douenel
+
+sur le repository local, vous créer un premier commit
+
+[15:21] Yvan Douenel
+
+vous faites correspondre ce repository local avec un repository distant sur github
+
+[15:22] Yvan Douenel
+
+Vous créer un répertoire html avec un fichier index.html
+
+[15:22] Yvan Douenel
+
+Vous créer une branche develop
+
+[15:22] Yvan Douenel
+
+Vous ajouter un ou deux collaborateurs sur github.
+
+[15:22] Yvan Douenel
+
+Vous demandez aux collaborateur d'ajouter du code js et/ou css
+
+[15:23] Yvan Douenel
+
+Vous récupérer leur code.
+
+[15:23] Yvan Douenel
+
+Vous faites exprès de générer un conflit en modifiant le code sur la même ligne
+
+[15:24] Yvan Douenel
+
+Le but est que votre page html profite de l'ajout des css et/ou du js
+
+A 16h45, chaque groupe fait une démo de son travail
+
+Vous devrez montrer, git log à l'appui, comment vos avez fonctionné
+
+
+
+
+*/
